4f519dfabbd5a094da77ab6a935e6720601309ef
Kaspars Dambis <hi@kaspars.net>
2023-10-12 13:45:57 +0300

    Rely on local Node tooling instead

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const ComposerPackage = require( './lib/composer-package' );
 const SubtreePackage = require( './lib/subtree-package' );
 
 module.exports = {
-	ComposerPackage,
 	SubtreePackage,
 };
